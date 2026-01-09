3785c821dfc72f555fcaa82c2cb75ec888de9e06
VladSunny <vladislavsunny@gmail.com>
2026-01-09 20:33:27 +0300

    reuse allowUnfree in flake

--- flake.nix
+++ flake.nix
@@ -19,6 +19,11 @@
     }:
     let
       system = "x86_64-linux";
+
+      pkgs = import nixpkgs {
+        inherit system;
+        config.allowUnfree = true;
+      };
     in
     {
       nixosConfigurations.pc-nixos = nixpkgs.lib.nixosSystem {
