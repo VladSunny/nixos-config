b12ef8ee30cd10493ec4fad5e60ef780984d6b7b
VladSunny <vladislavsunny@gmail.com>
2026-01-09 20:27:34 +0300

    fix

--- home-manager/fish.nix
+++ home-manager/fish.nix
@@ -3,8 +3,8 @@
     enable = true;
     shellAliases = {
       btw = "echo i use nixos btw";
-      nrs = "sudo nixos-rebuild switch --impure --flake /home/$USER/nixos-config#pc-nixos";
-      nup = "nix flake update --flake /home/$USER/nixos-config && sudo nixos-rebuild switch --impure --flake /home/$USER/nixos-config#pc-nixos";
+      nrs = "sudo nixos-rebuild switch --impure --flake /home/$USER/nixos-config#(hostname)";
+      nup = "nix flake update --flake /home/$USER/nixos-config && sudo nixos-rebuild switch --impure --flake /home/$USER/nixos-config#(hostname)";
     };
   };
 
